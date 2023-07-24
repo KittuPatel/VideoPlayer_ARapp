c20d6a32a92a7d7dfacb65b351a391f44821d67a
krishna.mangalarapu <krishna.mangalarapu@sri.com>
2023-07-23 19:06:45 -0700

    Initial Commit

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Krishna Mangalarapu on 7/23/23.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
